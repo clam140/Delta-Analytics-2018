4869b726c879cc6f665f13de83d3e9d0b053bb5f
Caleb Lam <clam140@gmail.com>
2018-06-23 20:17:44 -0700

    Change "cleaned" to raw file
    Each function cleans the data before calculating metric, so we should be reading in raw file

--- MR - Data Cleaning.py
+++ MR - Data Cleaning.py
@@ -10,5 +10,5 @@ import csv
 import copy
 import datetime as dt
 
-#Read cleaned file
-raw_mindright_df = pd.read_csv(r"C:\Users\Rasiga\Documents\MindRight\mindright-csv-export-staging-2018-05-08.csv")
+#Read raw file
+raw_mindright_df = pd.read_csv(r"mindright-csv-export-staging-2018-05-08.csv")
